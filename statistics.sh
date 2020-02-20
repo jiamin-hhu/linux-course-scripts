5b7f9fb88499d5ddce072ef3de687084b1388040
jiamin-hhu <jiamin.luu@hhu.edu.cn>
2020-02-20 17:10:20 +0800

    add the function to show the students who are online now.

--- statistics.sh
+++ statistics.sh
@@ -8,10 +8,11 @@ let numOfArgs++
 
 LIST=FALSE
 TOPN=10
+ONLE=FALSE
 
 while [ $# -eq 0 -o $numOfArgs -ne $OPTIND ]; do
 
-  getopts "hnt:" optKey
+  getopts "hnt:o" optKey
   if [ "$optKey" == "?" ]; then
     optKey="h"
   fi
@@ -21,14 +22,19 @@ while [ $# -eq 0 -o $numOfArgs -ne $OPTIND ]; do
 	   echo -en " -h Print this message and exit. \n\n"
 	   echo -en " -n Print the Number of the logged users. \n\n"
 	   echo -en " -t Print the name of the Top N users who have logged most. \n\n"
+	   echo -en " -o Print the students who are online. \n\n"
 	   exit 0;;
 	n)
 		LIST=TRUE	
+		;;
+	o)
+		ONLE=TRUE
 		;;
 	t)
 		PARA=${OPTARG/[0-9]*/}
 		if [ "$PARA" == "" ]; then 
 		  TOPN=$OPTARG
+		  LIST=TRUE
 		fi
 		;;
   esac
@@ -41,7 +47,12 @@ if [ "$LIST" == "TRUE" ]; then
   echo -en "Since $(uptime -s), in total $NUM student users have been logged.\n"
 fi
 
-if [ ! -z $TOPN ]; then
+if [ ! -z $TOPN -a "$LIST" == "TRUE" ]; then
   echo -en "\nThe top $TOPN students who logged most are: \n" 
   last -w | cut -d' ' -f1 | grep "^[0-9]" | sort | uniq -c | sort -nr | head -n ${TOPN} | awk -F' ' '{print $2}' | xargs -I {} grep {} ${bin}/names
+fi
+
+if [ "$ONLE" == "TRUE" ]; then
+  echo -en "\nThe students who are online now are: \n"
+  who -w | cut -d' ' -f1 | grep "^[0-9]" | sort | uniq -c | sort -nr | head -n ${TOPN} | awk -F' ' '{print $2}' | xargs -I {} grep {} ${bin}/names
 fi
