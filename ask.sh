365e232df5b60c3289d942ed3dfc184f5117da48
jiamin@Groot <jiamin.luu@hhu.edu.cn>
2023-02-18 16:10:31 +0800

    change the ask app and the name list

--- ask.sh
+++ ask.sh
@@ -5,5 +5,6 @@ bin=`dirname "$bin"`
 bin=`cd "$bin"; pwd`
 
 NAMEFILE=$bin/data/names
+LNUM=$(wc -l $NAMEFILE | cut -d' ' -f1)
 
-sed -n "$(( ${RANDOM} % 183 ))p" $NAMEFILE
+sed -n "$(( ${RANDOM} % ${LNUM} ))p" $NAMEFILE
