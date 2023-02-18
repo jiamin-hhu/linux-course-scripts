650ff0029c6ae5d7ff98fd393f27a8b8041cb7b6
jiamin@Groot <jiamin.luu@hhu.edu.cn>
2023-02-18 15:50:55 +0800

    add args and the user deletion

--- createBatchUser.sh
+++ createBatchUser.sh
@@ -6,37 +6,75 @@
 ## We can use :set fileformat=unix command to remove them in vi. 
 ##################################################
 
+declare -i numOfArgs=$#
+let numOfArgs++
+FILENAME=""
+OP=""
 
-if [ "$1" == "" ]; then
+while [ $# -eq 0 -o $numOfArgs -ne $OPTIND ]; do
+  getopts "ha:d:" optKey
+  if [ "$optKey" == "?" ]; then
+    optKey="h"
+  fi
+
+  case $optKey in
+        h) echo -en "\nUsuage of ${0##*/}:\n\n"
+           echo -en " -h Print this message and exit. \n\n"
+           echo -en " -a Add users listed in the given file. \n\n"
+           echo -en " -d Delete users listed in the given file. \n\n"
+           exit 0;;
+        a)
+                OP="add"
+                FILENAME="${OPTARG}"
+                ;;
+        d)
+                OP="del"
+                FILENAME="${OPTARG}"
+                ;;
+  esac
+done
+
+if [ "$FILENAME" == "" ]; then
   echo "Error! I need a student id list file "
   exit -1
 fi 
 
-if [ ! -f $1 ]; then
-  echo "Error! $1 is a invalid filename"
+if [ ! -f $FILENAME ]; then
+  echo "Error! $FILENAME is a invalid filename"
   exit -1
 fi
 
-listpath=$1
+listpath=$FILENAME
 listname=${listpath##*/}
 
 count=0
 
-for id in $(cat $listpath); do
-  un=$id 
-
-  # Indicate the user's group, shell, and create its home directory
-  sudo useradd -d /home/$un -m -g students -s /bin/bash $un
-
-  echo $un:$un | sudo chpasswd
-
-  sudo chage -d 0 $un
-  sudo cp $listpath /home/$un
-  sudo chown $un:students /home/$un/$listname
-  sudo chmod 600 /home/$un/$listname 
-  echo "Create user $un successfully!"
-  ((count++))
-done
-
-echo "In total create $count users"
-
+if [ "$OP" == "add" ]; then
+  for id in $(cat $listpath); do
+    un=$id 
+  
+    # Indicate the user's group, shell, and create its home directory
+    sudo useradd -d /home/$un -m -g students -s /bin/bash $un
+  
+    echo $un:$un | sudo chpasswd
+  
+    sudo chage -d 0 $un
+    sudo cp $listpath /home/$un
+    sudo chown $un:students /home/$un/$listname
+    sudo chmod 600 /home/$un/$listname 
+    echo "Create user $un successfully!"
+    ((count++))
+  done
+  
+  echo "In total create $count users"
+elif [ "OP" == "del" ]; then
+  for id in $(cat $listpath); do
+    sudo userdel -f -r $id
+    ((count++))
+  done
+  
+  echo "In total delete $count users"
+else
+  echo "Cannot recognize the operation $OP."
+  exit -2
+fi
