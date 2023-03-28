81edfe996c6730e68668a750a9ae81d62196b92e
jiamin@Groot <jiamin.luu@hhu.edu.cn>
2023-03-28 21:46:19 +0800

    update the ask app

--- ask.sh
+++ ask.sh
@@ -4,7 +4,12 @@ bin=`readlink -f "$0"`
 bin=`dirname "$bin"`
 bin=`cd "$bin"; pwd`
 
-NAMEFILE=$bin/data/names
+if [ "" != "$1" ] && [ -f $1 ]; then
+  NAMEFILE=$1
+else
+  NAMEFILE=$bin/data/names
+fi
+
 LNUM=$(wc -l $NAMEFILE | cut -d' ' -f1)
 
 sed -n "$(( ${RANDOM} % ${LNUM} ))p" $NAMEFILE
